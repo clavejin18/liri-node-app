d4ef26a1f14f959e20a1142671f6d6bd691213ae
Jaime Perez <clavejin18@gmail.com>
2019-05-05 16:04:07 -0700

    concert-this complete

--- liri.js
+++ liri.js
@@ -4,6 +4,9 @@ require("dotenv").config();
 // importing and storing keys
 var keys = require("./keys.js");
 
+// Setting moment variable
+var moment = require('moment');
+
 // spotify 
 //var spotify = new Spotify(keys.spotify);
 
@@ -16,7 +19,7 @@ var operand = inputString[2]
 // cases depending on the type of inputString
 switch (operand) {
     case 'concert-this':
-        console.log("you will be seraching for a concert")
+        concertSearch();
         break;
     case 'spotify-this-song':
         console.log("you will be searching for songs")
@@ -28,6 +31,49 @@ switch (operand) {
         console.log("you will be searching for something")
         break;
 }
+// concert function 
+function concertSearch() {
+    // Searching with axios
+    var axios = require(`axios`);
+
+    // Searching for concerts by artist
+    var artist = process.argv.slice(3).join(" ");
+
+    // URL for searching for concerts
+    var queryURl = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`
+
+    // Creating the concert request with axios to bandsintown API
+    console.log('----- Results -----');
+    console.log('');
+    console.log(`Artist/Band: ${artist}`);
+    console.log('')
+    axios.get(queryURl).then(function (response) {
+        var jsonData = response.data;
+        // A loop that displays all upcoming concerts
+        console.log('* Upcoming Events *');
+        console.log('');
+        for (index = 0, counter = 1; index < jsonData.length; index++ , counter++) {
+            console.log(`Event ${counter} :`)
+            var eventDays = [
+                "Venue: " + jsonData[index].venue.name,
+                "Venue Location: " + jsonData[index].venue.city + "," + jsonData[index].venue.country,
+                "Date Of Event: " + moment(jsonData[index].datetime).format("MM/DD/YYYY") + " at " + moment(jsonData[index].datetime).format('LT')
+            ].join("\n\n");
+            console.log(eventDays);
+            console.log('--------------');
+        }
+        console.log('----- End Of Results -----')
+    })
+
+        // documenting errors
+        .catch(function (error) {
+            console.log(error);
+        });
+
+
+
+}
+// Movie function
 function movie() {
     // requiring axios code
     var axios = require('axios')
@@ -39,7 +85,7 @@ function movie() {
     var queryUrl = `http://www.omdbapi.com/?t=${searchedMovie}&y=&plot=short&apikey=trilogy`
 
     // Creating the request with axios using the queryURl
-    console.log(`----- Results-----`)
+    console.log(`----- Results -----`)
     axios.get(queryUrl).then(function (response) {
         var jsonData = response.data;
         var showMovie = [
@@ -52,11 +98,11 @@ function movie() {
             "Actors: " + jsonData.Actors
         ].join("\n\n");
         console.log(showMovie)
-        console.log("----- End Of Results -----")   
+        console.log("----- End Of Results -----")
     })
 
-    // documenting errors
-    .catch(function (error) {
-        console.log(error);
-      });  
+        // documenting errors
+        .catch(function (error) {
+            console.log(error);
+        });
 }
